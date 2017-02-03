120d4d5ae05214dc3daac6aab786b03def557bcb
Pepe Valdivia <jvaldivia@softweb.pe>
2017-02-03 16:52:34 -0500

    Preubas unitarios QUnit BASE_UR + alumnos

--- public/test/alumno/index_test.js
+++ public/test/alumno/index_test.js
@@ -7,8 +7,6 @@ QUnit.module( "Ver lista de alumnos", {
 
 QUnit.test( "URL de eliminar debe coincidir", function( assert ) {
   	assert.equal(this.tabla.url_guardar, BASE_URL + "alumno/eliminar", "URL debe ser BASE_URL+ alumno/eliminar");
-  	console.log(this.tabla);
-  	console.log(this.tabla.array_json_btn_td.length);
 });
 
 QUnit.test( "La tabla debe tener 3 operaciones por fila", function( assert ) {
@@ -87,3 +85,47 @@ QUnit.test( "La tabla debe redireccionar a una nueva vista para editar un alumno
 
   	btn_editar.click();
 });
+
+QUnit.test( "Los eliminados al precionar el botón eliminar debe coincidir con en arreglo de eliminados de la tabla", function( assert ) {
+  	var tbody = $(this.id_tabla).children().eq(1);
+  	//var filas = $(this.id_tabla).children(0).children().eq(1).children().last().children().eq(3);
+  	var numero_filas = tbody.children().length;
+  	for(var k = numero_filas; tbody.children().length > 0; k = k - 1){
+  		var btn_eliminar = tbody.children().eq(k - 1).children().last().children().last();
+  		btn_eliminar.click();
+  	}
+
+  	//console.log(this.tabla.observador.arreglo_eliminados);
+
+  	var file = BASE_URL_TEST + "alumno/data/alumnos.txt";
+  	
+  	var rawFile = new XMLHttpRequest();
+    var allText;
+    
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function (){
+        if(rawFile.readyState === 4){
+            if(rawFile.status === 200 || rawFile.status == 0){
+                allText = rawFile.responseText;
+            }
+        }
+    }
+    
+    rawFile.send(null);
+
+    var ids_eliminados_txt = [];
+	var filas = allText.split("\n");
+
+
+	for(var k = filas.length ; k > 0 ; k = k - 1){
+	    var linea = filas[k-1].slice(0, -2);
+	    var datos = linea.split("::");
+	    ids_eliminados_txt.push(datos[0]);
+	}
+
+	//console.log(ids_eliminados_txt);
+	
+  	assert.deepEqual(ids_eliminados_txt, this.tabla.observador.arreglo_eliminados, "El arreglo this.tabla.observador.arreglo_eliminados es igual al generado al leer el archivo txt");
+});
+
+//console.log(tablaAlumno);
